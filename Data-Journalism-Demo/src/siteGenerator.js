a69b0d958da356cb1d3333c78f9a167e091634bf
jtrac521 <95438018+jtrac521@users.noreply.github.com>
2022-11-26 09:57:08 -0500

    map

--- Data-Journalism-Demo/src/siteGenerator.js
+++ Data-Journalism-Demo/src/siteGenerator.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ejs = require('ejs');
+const plotly = require('plotly');
 
 let country_info = JSON.parse(fs.readFileSync('../data/FossilFuelData.json', 'utf8'));
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
@@ -38,6 +39,7 @@ let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
   data: country_info
 });
+
 
 fs.writeFileSync('../public/index.html', index_html, 'utf8');
 
