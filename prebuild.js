1741edbf564d40a237d7626531ab74f339c5f20e
vgupta2024 <varun.gupta24@trinityschoolnyc.org>
2022-11-30 10:50:24 -0500

    done

--- prebuild.js
+++ prebuild.js
@@ -1,45 +1,35 @@
 const fs = require('fs');
 
-var oldPathVarunMicro = 'src/Varun_Design_Micro.jpg';
+var oldPathVarunMicro = 'Varun_Design_Micro.jpg';
 var newPathVarunMicro = 'public/Varun_Design_Micro.jpg';
-var topLevelPathVMic = '../Varun_Design_Micro.jpg';
 
 
 if (fs.existsSync(oldPathVarunMicro)) {
 fs.copyFile(oldPathVarunMicro, newPathVarunMicro, function (err) {
   if (err) throw err
 });
-fs.copyFile(oldPathVarunMicro, topLevelPathVMic, function (err) {
-  if (err) throw err
-});
 }
 
 
 
-var oldPathVarunMacro = 'src/Varun_Design_Macro.jpg';
+var oldPathVarunMacro = 'Varun_Design_Macro.jpg';
 var newPathVarunMacro = 'public/Varun_Design_Macro.jpg';
-var topLevelPathVMac = '../Varun_Design_Macro.jpg';
 
 
 if (fs.existsSync(oldPathVarunMacro)) {
 fs.copyFile(oldPathVarunMacro, newPathVarunMacro, function (err) {
   if (err) throw err
 });
-fs.copyFile(oldPathVarunMacro, topLevelPathVMac, function (err) {
-  if (err) throw err
-});
 }
 
-var oldPathJames = 'src/James_Design.jpg';
+
+var oldPathJames = 'James_Design.jpg';
 var newPathJames = 'public/James_Design.jpg';
-var topLevelPathJ = '../Varun_Design_Macro.jpg';
+
 
 
 
 if (fs.existsSync(oldPathJames)) {
-fs.copyFile(oldPathJames, newPathJames, function (err) {
-  if (err) throw err
-});
 fs.copyFile(oldPathJames, newPathJames, function (err) {
   if (err) throw err
 });
@@ -57,7 +47,7 @@ if (err) throw err
 });
 }
 
-var oldPipeline = 'src/pipeline.jpg';
+var oldPipeline = 'pipeline.jpg';
 var newPipeline = 'public/pipeline.jpg';
 
 if (fs.existsSync(oldPipeline)) {
